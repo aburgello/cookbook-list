c24d299f1c266d7dd206f47543ff8e64da55dd13
Antonio Burgello <burgello.antonio@gmail.com>
2024-09-30 10:47:49 +0100

    references

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :recipe_id, presence: true
+  validates :category_id, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :recipe, uniqueness: { scope: :category }
 end
