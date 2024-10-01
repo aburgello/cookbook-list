b0afca6a91fe0b9ee145aebfa424a8fb96d640bd
Antonio Burgello <burgello.antonio@gmail.com>
2024-10-01 10:23:27 +0100

    New categories pics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_112515) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_01_092245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_112515) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "recipes", force: :cascade do |t|
